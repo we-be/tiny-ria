470f2ea8b86534ca17038a41b65251beb8f5c4f9
hunter <hunterjsb@gmail.com>
2025-11-16 18:37:05 -0500

    feat: add schwab brokerage positions csv parser with template data

--- fiducia/schwab/csv/main.v
+++ fiducia/schwab/csv/main.v
@@ -1,5 +1,14 @@
 module main
 
+import encoding.csv
+
 fn main() {
-	println('Hello Charles Schwab!')
+	// date := "2025-11-16-165423"
+	date := "TEMPLATE"
+
+	mut parser := csv.new_reader_from_file("./data/Brokerage-Positions-${date}.csv")!
+	for {
+    items := parser.read() or { break }
+    println(items)
+	}
 }
